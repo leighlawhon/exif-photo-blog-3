0cd4da3ddc311f70121c8d0ba3663bb5990a476f
Leigh Stewardson <leighlawhon@gmail.com>
2024-07-04 08:52:14 -0400

    adding rylan tag

--- src/components/scenes/PanelLayout.tsx
+++ src/components/scenes/PanelLayout.tsx
@@ -66,6 +66,7 @@ export default function PanelLayout({
                                     </div>
                                 )}
                                 <div id={sceneTag} key={"panel-" + i} className={"panel-" + i + ", panel-border"}>
+                                    <p>{sceneTag}</p>
                                     {
                                         panelPhotos.map((photo, index) => (
                                         <Panel
